167b38278af03806c2ec263de3257916f7c4d4ed
Nos-Manus <nor-manus@protonmail.com>
2017-06-07 11:09:11 +0200

    change back peer amount

--- src/main/java/com/aidos/ari/service/PD.java
+++ src/main/java/com/aidos/ari/service/PD.java
@@ -56,7 +56,7 @@ public class PD {
 	private static final String PD_FILE = "peerlist.store.testnet";
 
 	// Number of nodes that should be added as peers
-	private static final int PEERS_TO_FIND = 3;
+	private static final int PEERS_TO_FIND = 6;
 
 	// Empty string for "false", or local ip in ipv4/6
 	private static Map<ipType, String> ipMode = new HashMap<ipType, String>();
