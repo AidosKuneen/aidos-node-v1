a790dee126f4e7ae6bf74419c6f8fafcf61f5527
Alexey Shokov <alexey@shockov.com>
2021-05-19 21:21:01 +0000

    Release preparation

--- src/main/java/com/aidos/ari/Main.java
+++ src/main/java/com/aidos/ari/Main.java
@@ -20,7 +20,7 @@ public class Main {
 	private static final Logger log = LoggerFactory.getLogger(Main.class);
 
 	public static final String NAME = "ARI";
-	public static final String VERSION = "1.0.1.0";
+	public static final String VERSION = "1.0.2.0";
 
 	public static void main(final String[] args) {
 
